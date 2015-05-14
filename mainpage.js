d5c3a3fb85ae32522a2092e689342bf90abeac0c
VSquidDevV <contact.devsquid@gmail.com>
2015-05-14 13:26:07 -0500

    Create mainpage.js

---
+++ mainpage.js
@@ -0,0 +1,9 @@
+function news(){
+document.getElementById("mainPageBody").src="Pages/news.html"
+}
+function home(){
+document.getElementById("mainPageBody").src="Pages/mainGame.html"
+}
+function hide(){
+document.getElementById("warning").style.display="none"
+}
